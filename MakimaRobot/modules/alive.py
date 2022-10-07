ef8bef2436d46ba21249ef9e6b185e6145ab3ec2
Manuindia122 <113814850+Manuindia122@users.noreply.github.com>
2022-10-07 19:37:08 +0530

    Update alive.py

--- MakimaRobot/modules/alive.py
+++ MakimaRobot/modules/alive.py
@@ -14,14 +14,14 @@ from MakimaRobot import OWNER_USERNAME, SUPPORT_CHAT, dispatcher
 from MakimaRobot import telethn as tbot
 
 PHOTO = [
-    "https://te.legra.ph/file/54acfd8a4213fabf997b2.jpg",
-    "https://te.legra.ph/file/6dfc25a7a32137e40eb4e.jpg",
+    "https://telegra.ph/file/413d85f75e2d012a77f90.jpg",
+    "https://telegra.ph/file/413d85f75e2d012a77f90.jpg",
 ]
 
 
 async def awake(event):
     TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ {dispatcher.bot.first_name}**\n━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝓚𝒶кคѕⒽᎥ ђ𝔞𝓉ᗩЌ𝒆 ⸙『𝕭𝖎𝖓𝖌𝖊』 ᭄™](https://t.me/{OWNER_USERNAME})** \n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [ＪＵＭＰＭＡＮ ＢＯＹ⁪⁬⁮](https://t.me/{OWNER_USERNAME})** \n\n"
     TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
     TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
     TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
